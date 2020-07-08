cdf96f30628a0d516c34b35c82fdd107c0e6900e
cessel <cessel@yandex.ru>
2020-07-08 18:21:23 +0300

    -- временное отключение перенапрвления на https

--- init.php
+++ init.php
@@ -10,13 +10,13 @@
 if(mb_substr($_SERVER['REQUEST_URI'],'-1',1) != '/'){
 	header('Location: '.$_SERVER['REQUEST_URI'].'/',true,301);
 }
-if (substr($_SERVER['HTTP_HOST'], 0, 4) == 'www.') {
-	$host = str_replace('www.','',$_SERVER['HTTP_HOST']);
-	$https = ($_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
-
-	$destination = $https.$host.$_SERVER['REQUEST_URI'];
-	header('Location: '.$destination,true,301);
-}
+//if (substr($_SERVER['HTTP_HOST'], 0, 4) == 'www.') {
+//	$host = str_replace('www.','',$_SERVER['HTTP_HOST']);
+//	$https = ($_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
+//
+//	$destination = $https.$host.$_SERVER['REQUEST_URI'];
+//	header('Location: '.$destination,true,301);
+//}
 
 require( dirname( __FILE__ ) . '/functions.php' );
 redirect_before();
