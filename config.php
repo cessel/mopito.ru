5f7bd27bf5748d7eb8e56f124d00b75cae68f5a7
cessel <cessel@yandex.ru>
2020-07-08 18:55:04 +0300

    -- добавлен отдельный файл для настроек ядра сайта

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Cessel's WEBGate Studio.
+ * User: Cessel
+ * Date: 08.07.2020
+ * Time: 18:34
+ */
+//$db_host = 'localhost';
+//$db_user = 'smsmoru2_mopito';
+//$db_password = 'Hs7DMc&V';
+//$database = 'smsmoru2_mopito_sql';
+
+$db_host = 'localhost';
+$db_user = 'cessel_alco';
+$db_password = 'Hs7DMc&V';
+$database = 'cessel_alco';
+
+define('DB_HOST',$db_host);
+define('DB_USER',$db_user);
+define('DB_PASS',$db_password);
+define('DB_NAME',$database);
+define('TABLE_NAME','alco_base_new');
